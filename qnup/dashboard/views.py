6d7c70e18b6bff849dda9b30927615f76a09e6a7
altyn-adam <danchik_1963@mail.ru>
2022-10-14 16:02:38 +0600

    upd#2

--- qnup/dashboard/views.py
+++ qnup/dashboard/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .form import TaskForm
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'dashboard/index.html', {
+        'title': 'Главная страниaца сайта',
+        'tasks': tasks
+    })
+
+def about(request):
+    return render(request, 'dashboard/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма не верная!'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'dashboard/create.html', context)
